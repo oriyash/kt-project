c1c2747dff45508ec7c6daa193aa8aacccdf96d0
Yash Ori <yash.ori498@gmail.com>
2022-11-29 17:16:55 +0000

    generating random position

--- src/TourBoard.js
+++ src/TourBoard.js
@@ -1,6 +1,12 @@
+// import { useState } from "react";
 import { Chessboard } from "react-chessboard";
+import { Tour } from "./tour/tour";
 
 export default function TourBoard() {
+    // const [tour, tourSet] = useState(new Tour());
+
+    const tour = new Tour();
+
     // create TourBoard Component
-    return <Chessboard />;
+    return <Chessboard position={tour.fen} />;
 }
--- src/tour/tour.js
+++ src/tour/tour.js
@@ -1,3 +1,22 @@
 export class Tour {
     // create tour classs
+    constructor() {
+        this.fen = this.createRandomFen();
+    }
+
+    createRandomFen() {
+        const rank = Math.floor(Math.random() * 8);
+        const file = Math.floor(Math.random() * 8);
+
+        const preKnight = "8/".repeat(rank);
+        const knight = file
+            ? file === 7
+                ? "7N/"
+                : `${file}N${7 - file}/`
+            : "N7/";
+        const postKnight = "8/".repeat(7 - rank);
+
+        const fen = `${preKnight}${knight}${postKnight}`.slice(0, -1);
+        return fen;
+    }
 }

120c96bfab10b81c9fb0028dd217dd42921fedfd
Yash Ori <yash.ori498@gmail.com>
2022-12-08 17:39:07 +0000

    added visited array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import TourBoard from "./TourBoard";
-
+import { useState } from "react";
+import { Chessboard } from "react-chessboard";
+import { Tour, makeNumber } from "./tour";
 function App() {
+    const [tour, setTour] = useState(new Tour());
+
+    const onDrop = (src, tgt) => {
+        console.log(`from ${src} to ${tgt}`);
+
+        tour.visited.push(makeNumber(tgt));
+        console.log(tour.visited);
+        // console.log(makeNumber(tgt));
+
+        // const tourCopy = { ...tour };
+        // console.log(tourCopy);
+
+        setTour(new Tour());
+    };
+
+    console.log(tour.fen);
+
     return (
         <div id="app">
-            <TourBoard />
+            <Chessboard position={tour.fen} onPieceDrop={onDrop} />
         </div>
     );
 }
--- src/tour.js
+++ src/tour.js
@@ -1,10 +1,73 @@
-export default class Tour {
+export class Tour {
     constructor() {
-        this.fen = makeFen();
+        const init = this.makeFen();
+        const initSq = 8 * init.rank + init.file;
+
+        this.fen = init.fen;
+        this.visited = [initSq];
     }
 
     makeFen() {
-        const rank = Math.floor(Math.random * 8);
-        const file = Math.floor(Math.random * 8);
+        const rank = Math.floor(Math.random() * 8);
+        const file = Math.floor(Math.random() * 8);
+
+        const preKnight = "8/".repeat(rank);
+        const knight = file
+            ? file === 7
+                ? "7N/"
+                : `${file}N${7 - file}/`
+            : "N7/";
+        const postKnight = "8/".repeat(7 - rank);
+
+        return {
+            fen: `${preKnight}${knight}${postKnight}`.slice(0, -1),
+            rank: rank,
+            file: file,
+        };
     }
+}
+
+export function makeNumber(coord) {
+    // const rank = coord[-1];
+    const rank = 7 - (Number(coord.slice(-1)) - 1);
+    let file = coord[0];
+
+    switch (file) {
+        case "a": {
+            file = 0;
+            break;
+        }
+        case "b": {
+            file = 1;
+            break;
+        }
+        case "c": {
+            file = 2;
+            break;
+        }
+        case "d": {
+            file = 3;
+            break;
+        }
+        case "e": {
+            file = 4;
+            break;
+        }
+        case "f": {
+            file = 5;
+            break;
+        }
+        case "g": {
+            file = 6;
+            break;
+        }
+        case "h": {
+            file = 7;
+            break;
+        }
+    }
+
+    // return { rank: rank, file: file };
+
+    return 8 * rank + file;
 }

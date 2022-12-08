d0552e50d44723ffc6905846240071309b17f1fc
Yash Ori <yash.ori498@gmail.com>
2022-12-08 15:46:30 +0000

    starting poitn

--- src/TourBoard.js
+++ src/TourBoard.js
@@ -1,16 +1,10 @@
 import { Chessboard } from "react-chessboard";
-import { Tour } from "./tour/tour";
 
 export default function TourBoard() {
-    const tour = new Tour();
-
     // create TourBoard Component
     return (
         <>
-            <Chessboard position={tour.fen} onPieceDrop={tour.onDrop} />
-            <button onClick={() => console.log(tour.visited)}>
-                See visited
-            </button>
+            <Chessboard position="start" />
         </>
     );
 }

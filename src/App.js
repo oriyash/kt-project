2283409b42d839748ff21e786952402a16d004fa
Yash Ori <yash.ori498@gmail.com>
2022-12-09 01:02:10 +0000

    middle of the board move validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import { Chessboard } from "react-chessboard";
-import { Tour, makeNumber } from "./tour";
+import { Tour } from "./tour";
 function App() {
     const [tour, setTour] = useState(new Tour());
 
     const onDrop = (src, tgt) => {
         console.log(`from ${src} to ${tgt}`);
 
-        tour.visited.push(makeNumber(tgt));
+        const move = tour.move(tgt);
+
         console.log(tour.visited);
+        console.log(tour.validMoves);
+
         // console.log(makeNumber(tgt));
 
         // const tourCopy = { ...tour };
         // console.log(tourCopy);
 
-        setTour(new Tour());
+        setTour(tour);
+        return move;
     };
 
-    console.log(tour.fen);
+    // console.log(tour.visited);
+    // console.log(tour.validMoves);
+
+    // console.(tour.fen);
 
     return (
         <div id="app">
--- src/tour.js
+++ src/tour.js
@@ -2,9 +2,19 @@ export class Tour {
     constructor() {
         const init = this.makeFen();
         const initSq = 8 * init.rank + init.file;
+        const initSqStr = makeStrCoord(init.rank, init.file);
+
+        console.log(initSqStr);
+
+        // console.log(`${init.rank} and ${init.file}`);
 
         this.fen = init.fen;
         this.visited = [initSq];
+        // this.validMoves = Array(64);
+        this.validMoves = this.updateValids(initSqStr, initSq);
+
+        console.log(this.validMoves);
+        console.log(this.visited);
     }
 
     makeFen() {
@@ -25,9 +35,41 @@ export class Tour {
             file: file,
         };
     }
+
+    move(tgtSt) {
+        const str = tgtSt;
+        const tgt = makeNumber(str);
+
+        if (!this.visited.includes(tgt) && this.validMoves.includes(tgt)) {
+            this.visited.push(tgt);
+            // TODO: updateFen() function
+            // this.updateFen()
+            this.validMoves = this.updateValids(str, tgt);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    updateValids(srcSt, src) {
+        const coords = makeCoord(srcSt);
+
+        const rank = coords.rank;
+        const file = coords.file;
+
+        const vectors = [-17, -15, -10, -6, 6, 10, 15, 17];
+        const cases = [0, 1, 6, 7];
+
+        if (!cases.includes(rank) && !cases.includes(file)) {
+            const res = vectors.map((val) => val + src);
+            return res;
+        } else {
+            return "f";
+        }
+    }
 }
 
-export function makeNumber(coord) {
+function makeNumber(coord) {
     // const rank = coord[-1];
     const rank = 7 - (Number(coord.slice(-1)) - 1);
     let file = coord[0];
@@ -67,7 +109,108 @@ export function makeNumber(coord) {
         }
     }
 
-    // return { rank: rank, file: file };
-
     return 8 * rank + file;
+}
+
+function makeCoord(coord) {
+    const rank = 7 - (Number(coord.slice(-1)) - 1);
+    let file = coord[0];
+
+    switch (file) {
+        case "a": {
+            file = 0;
+            break;
+        }
+        case "b": {
+            file = 1;
+            break;
+        }
+        case "c": {
+            file = 2;
+            break;
+        }
+        case "d": {
+            file = 3;
+            break;
+        }
+        case "e": {
+            file = 4;
+            break;
+        }
+        case "f": {
+            file = 5;
+            break;
+        }
+        case "g": {
+            file = 6;
+            break;
+        }
+        case "h": {
+            file = 7;
+            break;
+        }
+    }
+
+    return { rank: rank, file: file };
+}
+
+function makeStrCoord(rank, file) {
+    let ltrFile;
+    let reRank;
+
+    switch (file) {
+        case 0:
+            ltrFile = "a";
+            break;
+        case 1:
+            ltrFile = "b";
+            break;
+        case 2:
+            ltrFile = "c";
+            break;
+        case 3:
+            ltrFile = "d";
+            break;
+        case 4:
+            ltrFile = "e";
+            break;
+        case 5:
+            ltrFile = "f";
+            break;
+        case 6:
+            ltrFile = "g";
+            break;
+        case 7:
+            ltrFile = "h";
+            break;
+    }
+
+    switch (rank) {
+        case 0:
+            reRank = 8;
+            break;
+        case 1:
+            reRank = 7;
+            break;
+        case 2:
+            reRank = 6;
+            break;
+        case 3:
+            reRank = 5;
+            break;
+        case 4:
+            reRank = 4;
+            break;
+        case 5:
+            reRank = 3;
+            break;
+        case 6:
+            reRank = 2;
+            break;
+        case 7:
+            reRank = 1;
+            break;
+    }
+
+    return `${ltrFile}${reRank}`;
 }
